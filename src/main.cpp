4466ffc133c28078aadaf4c8cb5b11b68498c584
dridk <sacha@labsquare.org>
2022-01-19 01:47:43 +0100

    add boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <boost/algorithm/string.hpp>
 #include "vcfreader.h"
 int main(int argc, char *argv[])
 {
 
-    VcfReader reader("../examples/test.vep.vcf");
+
+    string test = "sacha le chat";
+
+    vector<string> items;
+    boost::split(items, test, boost::is_any_of(" "));
+
+    for (auto i: items)
+        cout<<"SPLIT: "<<i<<endl;
+
+//    VcfReader reader("../examples/test.vep.vcf");
 
 
-    for (auto i  : reader.get_samples())
-        cout<<"ici"<<i<<endl;
+//    for (auto i  : reader.get_samples())
+//        cout<<"ici"<<i<<endl;
 
     return 0;
 }
